bb9233ba7a8fa782f37a8386c9c49934743adba8
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-22 22:40:14 +0700

    Update Url

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("blogposts/", views.BlogPostListCreate.as_view(), name="blogpost-view-create"),
+    path("blogposts/", views.BlogPostListCreate.as_view(), name="blogpost-list-create"),
     path(
         "blogposts/<int:pk>/",
-        views.BlogPostRetrieveUpdateDestory.as_view(),
-        name="update",
+        views.BlogPostRetrieveUpdateDestroy.as_view(),
+        name="blogpost-retrieve-update-destroy",
     ),
 ]
